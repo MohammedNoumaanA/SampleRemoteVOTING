3974a3efd56a0f245f629887a4b33b2de859a5f0
“MohammedNoumaanA” <noumaanahamed@gmail.com>
2023-04-11 20:15:36 +0530

    Build added

--- voting/src/App.js
+++ voting/src/App.js
@@ -114,7 +114,7 @@ function App() {
 
   const RenderScreen = () => {
     return (
-      <div className="flex flex-col gap-4 items-center justify-center min-h-screen flex-wrap">
+      <div className="flex flex-col gap-4 items-center justify-center min-h-screen ">
         {screen === "addCandidate" ? (
           <AddCandidate setScreen={setScreen} addCandidate={addCandidate} />
         ) : screen === "vote" ? (
